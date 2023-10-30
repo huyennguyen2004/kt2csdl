3b292e76b4f97299529d29d5f28b0bdce23bfa8b
huyennguyen2004 <144600981+huyennguyen2004@users.noreply.github.com>
2023-10-30 23:26:19 +0700

    ktra2

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
 </section>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
+
 </body>
+
 </html>
