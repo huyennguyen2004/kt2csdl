34b1f737d7f3d170537151b426a06d172ae5f085
huyennguyen2004 <144600981+huyennguyen2004@users.noreply.github.com>
2023-10-30 23:16:39 +0700

    ktra2

--- db.php
+++ db.php
@@ -1,65 +1,72 @@
 <?php
-class Db
+class Db 
 {
-   private $servername = "localhost";
-   private   $user = "root";
-   private  $password = "";   
-   private $db="nguyenthibichhuyen";
-   private $conn=null;
-   public function _construct()
-   {
-    $this->conn = new mysqli($this->servername, $this->user, $this->password,$this->db);
-   }
-   //lay ra tat ca
-public function getALL()
-{
-    $sql = "SELECT id,name,image,status FROM 0058_brand WHERE status!=0";
-    $result = $this->conn->query($sql);
-    return $result->fetch_all(MYSQLI-ASSOC);
-}
-   //lay ra 1
-public function getOne($id)
-{
-   $sql="SELECT * FROM 0058_brand WHERE id='$id'";
-   $result = $this->conn->query($sql);
-   return $result->fetch_assoc();
-}   //so luong
-public function getCount()
-{
-    $sql = "SELECT id,name,image,status FROM 0058_brand WHERE status!=0";
-    $result = $this->conn->query($sql);
-}
-   //them
-public function insert($data)
-{
-   $strf="";
-   $strv="";
-   foreach($data as $f=>$v)
-   {
-      $strf.="$f,";
-      $strv.="'$v',";
-   }
-   $strf=rtrim(rtrim($strf),',');
-   $strv=rtrim(rtrim($strv),',');
-   $sql="INSERT INTO 0058_brand( $strf) VALUES( $strv)";
-   $this->conn->query($sql);
-}
-//sua
-public function update($data,$id)
-{
-   $strset="";
-   foreach($data as $f=>$v)
-   {
-      $strset.="$f,'$v',";
-   }
-   $strf=rtrim(rtrim($strset),',');
-   $sql="UPDATE 0058_brand SET $strset WHERE id='$id'";
-   $this->conn->query($sql);
-}
-   //xoa
-   public function delete($id)
-   {
-       $sql = "DELETE FROM 0058_brand WHERE id='$id'";
-       $this->conn->query($sql);
-   }
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $conn = null;
+    private $db = 'nguyenthibichhuyen';
+    public function __construct()
+    {
+        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
+    }
+    // Lấy ra tất cả
+    public function getAll()
+    {
+        $sql = "SELECT id, name, image, status FROM 0059_brand WHERE status != '0' ";
+        $result = $this->conn->query($sql);
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+    // Lấy ra một
+    public function getOne($id)
+    {
+        $sql = "SELECT * FROM 0059_brand WHERE id='$id' ";
+        $result = $this->conn->query($sql);
+        return $result->fetch_assoc();
+    }
+    // Số lượng
+    public function getCount()
+    {
+        $sql = "SELECT id, name, image, status FROM 0059_brand WHERE status != '0' ";
+        $result = $this->conn->query($sql);
+        return $result->num_rows;
+    }
+
+    // Thêm
+    public function insert($data)
+    {
+        $strf = "";
+        $strv = "";
+        foreach($data as $f=>$v)
+        {
+            $strf.="$f,";
+            $strv.="'$v',";
+        }
+        $strf = rtrim(rtrim($strf),',');
+        $strv = rtrim(rtrim($strv),',');
+        $sql = "INSERT INTO 0059_brand($strf) VALUES($strv)";
+        $this->conn->query($sql);
+
+    }
+    // Sửa
+    public function update($data,$id)
+    {
+        $strset = "";
+        foreach($data as $f=>$v)
+        {
+            $strset.=" $f='$v', ";
+        }
+        $strset = rtrim(rtrim($strset),',');
+        $sql = "UPDATE 0059_brand SET $strset WHERE id='$id' ";
+        $this->conn->query($sql);
+
+    }
+    // Xóa
+    public function delete($id)
+    {
+        
+        $sql = "DELETE FROM 0059_brand WHERE id='$id' ";
+        $this->conn->query($sql);
+
+    }
 }
